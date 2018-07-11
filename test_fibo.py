17220a28f6ae75d05288a659be0ca726e28913ee
Eva-Maria Gerstner <e.gerstner@fz-juelich.de>
2018-07-11 15:41:38 +0200

    Introduce Bug

--- test_fibo.py
+++ test_fibo.py
@@ -2,7 +2,7 @@ import fibo
 
 def test_fibo_10():
     result_10 = fibo.fib2(10)
-    expected_result = [0, 1, 1, 2, 3, 5, 8]
+    expected_result = [0, 1, 1, 2, 3, 5, 8, 10000]
     assert result_10 == expected_result
 
    
